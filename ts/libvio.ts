f447a56a27b2baf435860671bc865ac3109fd7e7
CxiaoyuN <2453223958@qq.com>
2025-09-26 23:20:28 +0800

    Update libvio.ts

--- ts/libvio.ts
+++ ts/libvio.ts
@@ -13,77 +13,76 @@ export default class libvio implements Handle {
 
   async getCategory() {
     return <ICategory[]>[
-      { text: '电影', id: '1' },
-      { text: '电视', id: '2' },
-      { text: '综艺', id: '3' },
-      { text: '动漫', id: '4' },
-      { text: '韩剧', id: '15' },
-      { text: '美剧', id: '16' },
+      { text: "电影", id: "1" },
+      { text: "电视剧", id: "2" },
+      { text: "综艺", id: "3" },
+      { text: "动漫", id: "4" },
+      { text: "韩剧", id: "15" },
+      { text: "美剧", id: "16" },
     ]
   }
 
   async getHome() {
-    const cate = env.get('category')
-    const page = env.get('page')
+    const cate = env.get("category")
+    const page = env.get("page")
     const url = `${env.baseUrl}/vodshow/${cate}--------${page}---.html`
     const html = await req(url)
     const $ = kitty.load(html)
-
-    return $('.stui-vodlist li').toArray().map<IMovie>(item => {
-      const a = $(item).find('a.stui-vodlist__thumb')
-      const id = a.attr('href') ?? ''
-      const title = a.attr('title') ?? ''
-      const cover = a.attr('data-original') ?? ''
-      const remark = a.find('.pic-text.text-right').text() ?? ''
+    return $(".stui-vodlist li").toArray().map<IMovie>(item => {
+      const a = $(item).find("a.stui-vodlist__thumb")
+      const id = a.attr("href") ?? ""
+      const title = a.attr("title") ?? ""
+      const cover = a.attr("data-original") ?? ""
+      const remark = a.find(".pic-text.text-right").text() ?? ""
       return <IMovie>{ id, title, cover, remark }
     })
   }
 
   async getDetail() {
-    const id = env.get('movieId')
+    const id = env.get("movieId")
     const url = `${env.baseUrl}${id}`
     const html = await req(url)
     const $ = kitty.load(html)
-
-    const tabs = $('.nav.nav-tabs li').toArray().map(item => {
-      return $(item).text().trim()
+    
+    const tabs = $(".nav.nav-tabs li").toArray().map(item => {
+      return $(item).text() ?? ""
     })
-
-    const map = $('.stui-panel_bd div.tab-pane').toArray().map(item => {
-      return $(item).find('a').toArray().map(_ => {
-        const text = $(_).text().trim()
-        const id = $(_).attr('href') ?? ''
+    
+    const map = $(".stui-panel_bd div.tab-pane").toArray().map(item => {
+      return $(item).find("a").toArray().map(_ => {
+        const text = $(_).text() ?? ""
+        const id = $(_).attr("href") ?? ""
         return <IPlaylistVideo>{ id, text }
       })
     })
-
+    
     const playlist = tabs.map((title, index) => {
       const videos = map[index]
       return <IPlaylist>{ title, videos }
     })
-
-    const a = $('.stui-pannel-box .stui-vodlist__thumb.picture.v-thumb')
-    const title = a.attr('title') ?? ''
-    const cover = a.find('img').attr('data-original') ?? ''
-    const desc = $('.detail.col-pd').text().trim()
-
+    
+    const a = $(".stui-pannel-box .stui-vodlist__thumb.picture.v-thumb")
+    const title = a.attr("title") ?? ""
+    const cover = a.find("img").attr("data-original") ?? ""
+    const desc = $(".detail.col-pd").text() ?? ""
+    
     return <IMovie>{ id, title, cover, desc, playlist }
   }
 
   async getSearch() {
-    const page = env.get<string>('page')
-    const wd = env.get<string>('keyword')
+    const page = env.get<string>("page")
+    const wd = env.get<string>("keyword")
     const url = `${env.baseUrl}/vodsearch/${wd}----------${page}---.html`
     const html = await req(url)
     const $ = kitty.load(html)
-
-    return $('.stui-vodlist__media li').toArray().map(item => {
-      const a = $(item).find('.v-thumb.stui-vodlist__thumb')
-      const title = a.attr('title') ?? ''
-      const cover = a.attr('data-original') ?? ''
-      const id = a.attr('href') ?? ''
-      const remark = a.find('.pic-text.text-right').text() ?? ''
-      return <IMovie>{ id, title, cover, remark }
+    
+    return $(".stui-vodlist__media li").toArray().map(item => {
+      const a = $(item).find(".v-thumb.stui-vodlist__thumb")
+      const title = a.attr("title") ?? ""
+      const cover = a.attr("data-original") ?? ""
+      const id = a.attr("href") ?? ""
+      const remark = a.find(".pic-text.text-right").text() ?? ""
+      return { id, title, cover, remark }
     })
   }
 
@@ -94,8 +93,8 @@ export default class libvio implements Handle {
 
 // TEST
 // const env = createTestEnv("https://www.libvio.cc")
-// const tv = new libvio()
-// ;(async () => {
+// const tv = new libvio();
+// (async () => {
 //   const cates = await tv.getCategory()
 //   env.set("category", cates[0].id)
 //   env.set("page", 1)

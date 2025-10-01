6db70d6b610fdd1d370f8d87ea87b3c16a50de4f
CxiaoyuN <2453223958@qq.com>
2025-10-01 19:50:02 +0800

    Update 4hu.ts

--- x18/4hu.ts
+++ x18/4hu.ts
@@ -2,7 +2,7 @@ export default class FourHu implements Handle {
   getConfig() {
     return {
       id: "4hu",
-      name: "四虎成人",
+      name: "四虎成人测试",
       api: "https://www.4hu.tv",
       type: 1,
       nsfw: true

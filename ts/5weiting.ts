16d097935a4b4dba50c252e092db5bac29c1085e
CxiaoyuN <2453223958@qq.com>
2025-10-09 21:08:12 +0800

    Update and rename 6weiting.ts to 5weiting.ts

--- ts/5weiting.ts
+++ ts/5weiting.ts
@@ -1,7 +1,7 @@
 export default class LiuYueTingShu implements Handle {
   getConfig() {
     return {
-      id: '6weiting',
+      id: '5weiting',
       name: '六月听书',
       type: 1,
       api: 'http://www.5weiting.com',
@@ -13,30 +13,11 @@ export default class LiuYueTingShu implements Handle {
     return [
       { text: '玄幻奇幻', id: '/ys/t1' },
       { text: '修真武侠', id: '/ys/t2' },
-      { text: '恐怖灵异', id: '/ys/t3' },
-      { text: '古今言情', id: '/ys/t4' },
       { text: '都市言情', id: '/ys/t28' },
+      { text: '恐怖灵异', id: '/ys/t3' },
       { text: '穿越重生', id: '/ys/t5' },
-      { text: '粤语古仔', id: '/ys/t6' },
-      { text: '网游小说', id: '/ys/t7' },
-      { text: '评书大全', id: '/ys/t8' },
-      { text: '相声小品', id: '/ys/t9' },
-      { text: '百家讲坛', id: '/ys/t10' },
-      { text: '通俗文学', id: '/ys/t11' },
       { text: '历史纪实', id: '/ys/t12' },
-      { text: '军事', id: '/ys/t13' },
-      { text: '悬疑推理', id: '/ys/t14' },
-      { text: '官场商战', id: '/ys/t15' },
-      { text: '儿童读物', id: '/ys/t16' },
-      { text: '广播剧', id: '/ys/t17' },
-      { text: 'ebc5系列', id: '/ys/t18' },
-      { text: '商业', id: '/ys/t19' },
-      { text: '生活', id: '/ys/t20' },
-      { text: '教材', id: '/ys/t21' },
-      { text: '外文原版', id: '/ys/t22' },
-      { text: '期刊杂志', id: '/ys/t23' },
-      { text: '脱口秀', id: '/ys/t27' },
-      { text: '戏曲', id: '/ys/t24' }
+      { text: '广播剧', id: '/ys/t17' }
     ]
   }
 
@@ -105,10 +86,28 @@ export default class LiuYueTingShu implements Handle {
   async parseIframe() {
     const iframe = env.get('iframe')
     const html = await req(`${env.baseUrl}${iframe}`)
-    const match = html.match(/"(http:\/\/61\\.160\\.194\\.89:20001\\/[^"]+\\.mp3[^"]*)"/)
+    const match = html.match(/"(http:\/\/61\.160\.194\.89:20001\/[^"]+\.mp3[^"]*)"/)
     if (match) {
       return { type: 'mp3', url: match[1] }
     }
     return null
+  }
+}
+
+export const entry: Iconfig = {
+  id: '5weiting',
+  name: '六月听书网',
+  type: 1,
+  api: 'http://www.5weiting.com',
+  nsfw: false,
+  extra: {
+    js: {
+      category: 'getCategory',
+      categoryDetail: 'getCategoryDetail',
+      home: 'getHome',
+      search: 'getSearch',
+      detail: 'getDetail',
+      parseIframe: 'parseIframe'
+    }
   }
 }

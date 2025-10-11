e4b8488c149bafed0cfa15cb505cd02621c5b513
CxiaoyuN <2453223958@qq.com>
2025-10-11 14:59:24 +0800

    Update xm618208.ts

--- x18/xm618208.ts
+++ x18/xm618208.ts
@@ -1,11 +1,11 @@
-export default class XM618208 implements Handle {
+export default class XM6181035 implements Handle {
   getConfig() {
     return {
-      id: 'xm618208',
+      id: 'xm6181035',
       name: '黄集资源',
-      api: 'https://618208.xyz',
+      api: 'https://6181035.xyz',
       type: 1,
-      nsfw: false
+      nsfw: true
     }
   }
 
@@ -13,7 +13,22 @@ export default class XM618208 implements Handle {
     return [
       { id: '13', text: '香蕉' },
       { id: '22', text: '制服' },
-      { id: '6', text: '国产' }
+      { id: '6', text: '国产' },
+      { id: '8', text: '少女' },
+      { id: '9', text: '辣妹' },
+      { id: '10', text: '女同' },
+      { id: '11', text: '素人' },
+      { id: '12', text: '角色' },
+      { id: '20', text: '人妻' },
+      { id: '23', text: '日韩' },
+      { id: '21', text: '伦理' },
+      { id: '7', text: '动漫' },
+      { id: '14', text: '二区' },
+      { id: '40', text: '三区' },
+      { id: '52', text: '无码' },
+      { id: '33', text: '中文' },
+      { id: '44', text: '传媒' },
+      { id: '32', text: '自拍' }
     ]
   }
 
@@ -42,8 +57,8 @@ export default class XM618208 implements Handle {
     const title = $('p.km-script').text().trim()
     const cover = $('img.lazy').attr('data-original') ?? ''
     const playlist = [{
-      name: '播放',
-      urls: [{ name: '播放地址', id }]
+      name: '在线播放',
+      urls: [{ name: '立即播放', id: url }]
     }]
 
     return { id, title, cover, desc: '', remark: '', playlist }
